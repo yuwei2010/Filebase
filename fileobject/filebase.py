970a5cf81deda85061efb08597451f714b149c16
yuwei2010 <34447427+yuwei2010@users.noreply.github.com>
2019-03-25 20:13:05 +0100

    Add files via upload

--- fileobject/filebase.py
+++ fileobject/filebase.py
@@ -27,6 +27,12 @@ class FileBase(FileList):
     @staticmethod
     def list_files(root, *, depth=None):
         
+        '''
+        list all files in the root folder.
+        
+        depth: 
+        '''
+        
         count = 0
         
         for rt, dirs, files in os.walk(root):
@@ -34,7 +40,8 @@ class FileBase(FileList):
             if depth and count > depth:
                 break
             
-            
+            for f in files:
+                print(os.path.normpath(os.path.join(rt, f)))
             
             depth += 1
             
